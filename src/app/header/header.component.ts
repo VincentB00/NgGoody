605ef1650c35d0bdec99560c4af6df8a25a9cf61
Vincent Bui <vincentbuiwork@gmail.com>
2022-08-07 00:14:24 -0400

    BETA 0.1.0

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/service/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(public authService: AuthService) { }
+
+  ngOnInit() 
+  {
+    this.authService.updateUser();
+  }
+
+  getUserName(): string
+  {
+    let user = this.authService.getUser();
+    return user.first_name + " " + user.last_name;
+  }
+
+  signOut()
+  {
+    this.authService.logout()
+    .subscribe(
+      res => this.authService.clearUser(),
+      error => this.authService.clearUser()
+    );
+  }
+}
